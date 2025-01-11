90736c8985bca7ef87398a7e532e0143cfec416b
tanxzh <tanxzh@nationalchip.com>
2025-01-11 20:05:51 +0800

    v0.1

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,14 +12,17 @@ map("n", "ee", "<cmd>Vista!!<CR>", { desc = "Toggle Vista", silent = true })
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvimtree", silent = true })
 
 -- LSP 导航
-map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = "跳转到定义" })
-map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { desc = "查看引用" })
+map("n", "gd", "<cmd>Telescope tags<CR>", { desc = "跳转到定义" })
+map("n", "gr", "<cmd>Telescope tags { only_sort_tags = true, fname_width = 60, show_line = true }<CR>", { desc = "查找引用" })
+map("n", "gw", "<cmd>Telescope grep_string<CR>", { desc = "全局查找当前单词" })
 map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { desc = "显示文档" })
 map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { desc = "重命名" })
 
 -- Tags 导航
 map("n", "<C-]>", "g<C-]>", { desc = "跳转到定义(多选)" })
+map("n", "gd", "g<C-]>", { desc = "跳转到定义(多选)" })
 map("n", "<C-t>", "<C-o>", { desc = "返回上一位置" })
+map("n", "gr", "g]", { desc = "查看引用" })
 
 -- 高亮当前单词
 map("n", "\\m", "<Plug>MarkSet", { desc = "高亮当前单词" })
